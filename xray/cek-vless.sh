da9cdacd960e0a0689ed2339f97bc8262649f18a
ZEAKING X DXT <129081220+Zeadxt@users.noreply.github.com>
2023-06-17 22:50:13 +0700

    Update cek-vless.sh

--- xray/cek-vless.sh
+++ xray/cek-vless.sh
@@ -22,7 +22,7 @@ exit 0
 fi
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/config.json | grep '^#vl#' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vless User Login ]=---------";
 echo "----------------------------------------";
